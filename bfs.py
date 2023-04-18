e6f50b31876e60ce9257c0732c5eb88c5f4b3923
hanoo2002 <hana.ghanem@gmail.com>
2023-04-18 02:06:08 +0200

    BFS

---
+++ bfs.py
@@ -0,0 +1,27 @@
+def printLevelOrder(root):
+    # Base Case
+    if root is None:
+        return
+
+    # Create an empty queue
+    # for level order traversal
+    queue = []
+
+    # Enqueue Root and initialize height
+    queue.append(root)
+
+    while (len(queue) > 0):
+
+        # Print front of queue and
+        # remove it from queue
+        print(queue[0].data, end=" ")
+        node = queue.pop(0)
+        print(node)
+
+        # Enqueue left child
+        if node.left is not None:
+            queue.append(node.left)
+
+        # Enqueue right child
+        if node.right is not None:
+            queue.append(node.right)
